fd98144d1340b3ee48e285a15454c5288f0ab486
cavanfarrell <cavan.farrell@icloud.com>
2020-03-30 11:34:17 -0500

    migration

---
+++ AppStoreScraperModels/Migrations/20200328001841_AddingDateLoaded.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AppStoreScraperModels.Migrations
+{
+    public partial class AddingDateLoaded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateLoaded",
+                table: "Reviews",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateLoaded",
+                table: "Reviews");
+        }
+    }
+}
